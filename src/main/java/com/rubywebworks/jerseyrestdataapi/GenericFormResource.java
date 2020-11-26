b95a1d54fb56b5fafa6ee538a7a0cd8e79a3099c
Hani Elabed <hani.elabed@gmail.com>
2020-11-25 20:30:45 -0600

    uses @CookieParam and @HeaderParam.

--- src/main/java/com/rubywebworks/jerseyrestdataapi/GenericFormResource.java
+++ src/main/java/com/rubywebworks/jerseyrestdataapi/GenericFormResource.java
@@ -1,13 +1,16 @@
 package com.rubywebworks.jerseyrestdataapi;
 
 
+import javax.ws.rs.CookieParam;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -32,5 +35,34 @@ public class GenericFormResource {
                .status(200)
                .entity("Id: " + id + ", Name: " + aName + ", Age: " + age + ", Height: " + height + ", Weight: " + weight)
                .build();
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("userAgent")
+  // /api/forms/userAgent
+  public String getUserDevice(
+          @HeaderParam("user-agent") String userAgent,
+          @HeaderParam("Content-Type") MediaType contentType) {
+    return "User Agent: " + userAgent + ",\nContent-Type: " + contentType ;
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("browserCookies")
+  // /api/forms/browserCookies
+  public String browserCookies(
+          @CookieParam("user-agent") Cookie userAgentCookie) {
+    if( userAgentCookie != null ) {
+      return
+      "\nName: "    + userAgentCookie.getName() +
+      "\nValue: "   + userAgentCookie.getValue() +
+      "\nDomain: "  + userAgentCookie.getDomain() +
+      "\nPath: "    + userAgentCookie.getPath() +
+      "\nVersion: " + userAgentCookie.getVersion();
+
+    } else {
+     return "userAgentCookie is null";
+    }
   }
 }
